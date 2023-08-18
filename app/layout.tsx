ae84b24717603f992b6ec803bf93a89f501aee70
Samuel Chen <samuelchen0606@gmail.com>
2023-08-18 15:23:43 +1000

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import WalletProvider from './(providers)/wallet-provider'
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
         </WalletProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
